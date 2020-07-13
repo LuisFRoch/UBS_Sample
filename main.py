11ab0be8f00a3db8ee0b43f9ccf7964099d8bcf0
MiniGeek <29665541+LuisFRoch@users.noreply.github.com>
2020-07-13 11:32:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "OperationalError",
-     "evalue": "no such table: CreditRisk",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[0;31mOperationalError\u001b[0m                          Traceback (most recent call last)",
-      "\u001b[0;32m<ipython-input-2-8ea00d7a9ae7>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m\u001b[0m\n\u001b[1;32m     23\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     24\u001b[0m \u001b[0;32mif\u001b[0m \u001b[0m__name__\u001b[0m \u001b[0;34m==\u001b[0m \u001b[0;34m'__main__'\u001b[0m\u001b[0;34m:\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m---> 25\u001b[0;31m     \u001b[0mmain\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m",
-      "\u001b[0;32m<ipython-input-2-8ea00d7a9ae7>\u001b[0m in \u001b[0;36mmain\u001b[0;34m()\u001b[0m\n\u001b[1;32m     15\u001b[0m     \u001b[0mcursor\u001b[0m \u001b[0;34m=\u001b[0m \u001b[0mdb\u001b[0m\u001b[0;34m.\u001b[0m\u001b[0mcursor\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     16\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m---> 17\u001b[0;31m     \u001b[0;32mfor\u001b[0m \u001b[0mrow\u001b[0m \u001b[0;32min\u001b[0m \u001b[0mcursor\u001b[0m\u001b[0;34m.\u001b[0m\u001b[0mexecute\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m'SELECT * from CreditRisk'\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m:\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m\u001b[1;32m     18\u001b[0m       \u001b[0mprint\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0mrow\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     19\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n",
-      "\u001b[0;31mOperationalError\u001b[0m: no such table: CreditRisk"
-     ]
-    }
-   ],
-   "source": [
-    "#\n",
-    "# Pre-requisites:\n",
-    "# Python2.5+ with sqlite3  - pysqlite is available in your python installation (included in the standard library since Python 2.5)\n",
-    "#\n",
-    "# This script will print 10 rows from teams and users table\n",
-    "#\n",
-    "\n",
-    "\n",
-    "import sqlite3 # assumes you already have sqlite3 \n",
-    " \n",
-    "\n",
-    "def main():\n",
-    "\n",
-    "    db = sqlite3.connect('CreditRisk.db')\n",
-    "    cursor = db.cursor()\n",
-    "\n",
-    "    for row in cursor.execute('SELECT * from CreditRisk'):\n",
-    "      print(row)\n",
-    "\n",
-    "    for row in cursor.execute('SELECT * from LEI_code'):\n",
-    "      print(row)\n",
-    "\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    main()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "hide_input": false,
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  },
-  "toc": {
-   "base_numbering": 1,
-   "nav_menu": {},
-   "number_sections": false,
-   "sideBar": false,
-   "skip_h1_title": false,
-   "title_cell": "Table of Contents",
-   "title_sidebar": "Contents",
-   "toc_cell": false,
-   "toc_position": {},
-   "toc_section_display": false,
-   "toc_window_display": false
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#
+# Pre-requisites:
+# Python2.5+ with sqlite3  - pysqlite is available in your python installation (included in the standard library since Python 2.5)
+#
+# This script will print 10 rows from teams and users table
+#
+
+
+import sqlite3 # assumes you already have sqlite3 
+ 
+
+def main():
+
+    db = sqlite3.connect('CreditRisk.db')
+    cursor = db.cursor()
+
+    for row in cursor.execute('SELECT * from Credit_risk'):
+      print(row)
+
+    for row in cursor.execute('SELECT * from EBA_List'):
+      print(row)
+
+
+if __name__ == '__main__':
+    main()
